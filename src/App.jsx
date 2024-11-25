248a0dbbc65db878ff506b6fcd1f5b1e76c99671
Shirish Koirala <shirishkoirala@gmail.com>
2024-11-25 15:25:16 +1000

    Working with state.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { useState } from 'react';
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcepts.jsx';
+import TabButton from './components/TabButton.jsx';
 
 function App() {
+  const [ selectedTopic, setSelectedTopic ] = useState('Please click a button');
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
   return (
     <div>
       <Header />
@@ -16,6 +23,16 @@ function App() {
               })
             }
           </ul>
+        </section>
+        <section id='examples'>
+            <h2>Examples</h2>
+            <menu>
+              <TabButton onClick={() => handleSelect('components')}>Components</TabButton>
+              <TabButton onClick={() => handleSelect('jsx')}>JSX</TabButton>
+              <TabButton onClick={() => handleSelect('props')}>Props</TabButton>
+              <TabButton onClick={() => handleSelect('state')}>State</TabButton>
+            </menu>
+            {selectedTopic}
         </section>
       </main>
     </div>
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,3 @@
+export default function TabButton({children, onClick}) {
+    return (<li><button onClick={onClick}>{children}</button></li>);
+}
