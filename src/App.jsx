bc2db2ac33e951c47b8424ebc111494102128ee2
Shirish Koirala <shirishkoirala@gmail.com>
2024-11-25 12:59:05 +1000

    CoreConcept Component Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,51 @@
+import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data.js';
+const reactDescriptions = ['Fundamental', "Crutial", 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1))
+}
+
 function Header() {
+  const description = reactDescriptions[genRandomInt(reactDescriptions.length - 1)]
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
   );
 }
+function CoreConcept({title, description, image}) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
 function App() {
+
   return (
     <div>
-      <Header/>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {
+              CORE_CONCEPTS.map((coreConcept) => {
+                return <CoreConcept {...coreConcept} />
+              })
+            }
+          </ul>
+        </section>
       </main>
     </div>
   );
