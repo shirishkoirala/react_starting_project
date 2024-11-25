81e0766fae8e54c9c9e122b0863b770e192474a9
Shirish Koirala <shirishkoirala@gmail.com>
2024-11-26 09:38:37 +1000

    Setting Components Dynamically

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,11 @@
+export default function Tabs({ children, buttons, buttonsContainer }) {
+    const ButtonsContainer = buttonsContainer
+    return (
+        <>
+            <ButtonsContainer>
+                {buttons}
+            </ButtonsContainer>
+            {children}
+        </>
+    );
+}
